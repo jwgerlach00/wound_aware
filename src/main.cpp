45e480927e6e60efc2431c6e6f9f607c382f5c6c
Jacob Gerlach <jwgerlach00@gmail.com>
2023-03-07 21:43:57 -0500

    Cleanup and minor logic changes

--- src/main.cpp
+++ src/main.cpp
@@ -3,115 +3,146 @@
 
 unsigned long startTime;
 unsigned long loopStartTime;
+unsigned long currentTime;
+
+int pwrLEDPin = PD6;
+int tempLEDPin = PD7;
+int moistLEDPin = PB0;
+int timeLEDPin = PB1;
+int buzzPin = PB2;
+
+int pwrAddress = 0;
+int impedanceAddress = 1;
+int tempAddress = 2;
+
+int fiveMins = 5*60*1000;
+int seventyTwoHours = 72*60*60*1000;
+
+int impedanceThres = 50000; // 50kOhms
+int impedanceWindowSize = 12; // 60mins / 5mins
+int tempWindowSize = 12; // 60mins / 5mins
+
 
 void setup() {
-  // Put your setup code here, to run once:
-  // Wait for power button to be pressed
+  if (EEPROM.read(pwrAddress) == 0) { // If turned on previously; default EEPROM is 255
+    exit(0); // Break loop
+  }
+
   // Sample the start time (for the 72 hour limit)
   startTime = millis();
 
-  // Connect pins to LEDs
-  pinMode( PD6, OUTPUT); // Power indicator light
-  pinMode( PD7, OUTPUT); // Temperature indicator light
-  pinMode( PB0, OUTPUT); // Moisture indicator light
-  pinMode( PB1, OUTPUT); // Time limit indicator light
-  pinMode( PB2, OUTPUT); // Buzzer for auditory indication
+  // Connect pins to LED indicators
+  pinMode(pwrLEDPin, OUTPUT);
+  pinMode(tempLEDPin, OUTPUT);
+  pinMode(moistLEDPin, OUTPUT);
+  pinMode(timeLEDPin, OUTPUT);
+  pinMode(buzzPin, OUTPUT);
 
-  while (EEPROM.read(1) == 1) { //This means it was turned off previously
-    // Don't let anything turn on
-    // Infinite loop
-  }
-  
-  // Turn on power indicator light
-  digitalWrite(<PowerLEDNumber>, HIGH);
+  // if (EEPROM.read(pwrAddress) == 255) { // If turned on previously
+  //   powerOffDevice();
+  // } else {
+  digitalWrite(pwrLEDPin, HIGH); // Turn on power indicator light
+  EEPROM.write(pwrAddress, 0); // Mark as turned on
 }
 
 void loop() {
-  // Sample start time at beginning (for 5 minute sampling loop)
-  loopStartTime = millis();
-  measure_impedance();
-  measure_temperature();
-  check_72_hour();
-  
-  // Loop here while waiting for 5 minutes to sample again
-  unsigned long currentTime = millis();
-  while (currentTime < (loopStartTime + 300000)) {
-    // If power button has been pressed
-    if (digitalRead(powerButtonPin) == LOW) {
-      powerOffDevice();
-    }
-    currentTime = millis();
+  currentTime = millis();
+  if (currentTime >= (loopStartTime + fiveMins)) { // Only enter if its been 5 mins since last run
+    loopStartTime = millis(); // Reset
+    main();
   }
 }
 
-void measure_impedance() {
-  // TODO: FIX ME Measure impedance
-  
+void main() {
+  measureImpedance();
+  measureTemp();
+  check72Hour();
+}
+
+// void powerOffDevice() {
+//   // Turn off buzzers and LED lights
+//   digitalWrite(pwrLEDPin, LOW);
+//   digitalWrite(tempLEDPin, LOW);
+//   digitalWrite(moistLEDPin, LOW);
+//   digitalWrite(timeLEDPin, LOW);
+//   digitalWrite(buzzPin, LOW);
+// }
+
+float calcImpedance() {
+  // TODO: FIX ME
+}
+
+void measureImpedance() {
   // If impedance is greater than 50kOhms
-  if (impedance > 50000) {
-    if (EEPROM.read(1) == 255) { // This means this is the first measurement
-      EEPROM.write(1, 1);
+  if (calcImpedance() > impedanceThres) {
+    if (EEPROM.read(impedanceAddress) == 255) { // This means this is the first measurement (empty EEPROM)
+      EEPROM.write(impedanceAddress, 1);
     } else { // Increment impedance count
-      EEPROM.write(1, EEPROM.read(1)+1);
+      EEPROM.write(impedanceAddress, EEPROM.read(1) + 1);
     }
     // If impedance count is greater than or equal to 12
-    if (EEPROM.read(1) >= 12) {
+    if (EEPROM.read(impedanceAddress) >= impedanceWindowSize) {
       // Turn on moisture indicator light and audio signal
-      digitalWrite( PB0 , HIGH);
-      digitalWrite( PB2 , HIGH);
+      digitalWrite(moistLEDPin, HIGH);
+      digitalWrite(buzzPin, HIGH);
     }
   }
   else { // Clear impedance counts if impedance within proper range
-    EEPROM.write(1, 0);
+    EEPROM.write(impedanceAddress, 0);
   }
 }
 
-void measure_temperature() {
+float tempConversion(float temp) {
+  // TODO: FIX ME
+}
+
+float calcTempDifferential() {
+  // TODO: FIX ME
+  int periwoundTemp = NULL;
+  int woundTemp = NULL;
+  abs(tempConversion(periwoundTemp - woundTemp));
+}
+
+void measureTemp() {
   // TODO: FIX ME Measure temperature
       // Measure periwound temperature
       // Measure wound temperature
       // Calculate temperature difference
   // If differential measurement increased by greater than 
   // or equal to 1.5C
-  if (differential >= 1.5) {
+
+  if (calcTempDifferential() >= 1.5) {
     // Increment temperature count
-    if (EEPROM.read(2) == 255) { // This means this is the first measurement
-      EEPROM.write(2, 1);
+    if (EEPROM.read(tempAddress) == 255) { // This means this is the first measurement (empty EEPROM)
+      EEPROM.write(tempAddress, 1);
     } else { // Increment temperature count
-      EEPROM.write(2, EEPROM.read(2)+1);
+      EEPROM.write(tempAddress, EEPROM.read(tempAddress) + 1);
     }
     // If temperature count is greater than or equal to 12
     // Turn on temperature indicator light and audio signal
-    if (EEPROM.read(2) >= 12) {
+    if (EEPROM.read(tempAddress) >= tempWindowSize) {
       // Turn on temperature indicator light and audio signal
-      digitalWrite( PD7 , HIGH);
-      digitalWrite( PB2 , HIGH);
+      digitalWrite(tempLEDPin, HIGH);
+      digitalWrite(buzzPin, HIGH);
     }
   } else { // Clear temperature count
-    EEPROM.write(2, 0);
+    EEPROM.write(tempAddress, 0);
   }
 }
 
-void check_72_hour() {
+void check72Hour() {
   // Check if device on time has reached 72 hour limit
-  if( millis() >= (startTime + 259200000) ) {
-    // Turn on change dressing indicator light and audio signal
-    digitalWrite( PB1, HIGH);
-    digitalWrite( PB2 , HIGH);
+  if(currentTime >= (startTime + seventyTwoHours) ) {
+    while (millis() - currentTime < fiveMins) { // Signal for 5 mins
+      digitalWrite(timeLEDPin, HIGH);
+      digitalWrite(buzzPin, HIGH);
+    }
+    turnOff();
   }
 }
 
-void powerOffDevice() {
-  EEPROM.write(0, 1);
-  // Turn off buzzers and LED lights
-  digitalWrite( PD6, LOW); // Power indicator light
-  digitalWrite( PB0, LOW); // Moisture indicator light
-  digitalWrite( PD7, LOW); // Temperature indicator light
-  digitalWrite( PB1, LOW); // Time limit indicator light
-  digitalWrite( PB2 , LOW); // Buzzer for auditory indication
-  // TODO: FIX ME, maybe a more efficient way to turn off...
-  while (EEPROM.read(1) == 1) {
-    // Don't let anything turn on
-    // Infinite loop
-  }
+void turnOff() {
+  EEPROM.read(pwrAddress) == 0; // Mark as turned off
+  exit(0);
 }
+
